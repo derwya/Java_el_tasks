30e030eec8d34c35cb941b2db4038385e3426a13
derwya <39012104+derwya@users.noreply.github.com>
2019-11-30 10:15:42 +0200

    update

--- src/com/alex/lesson5/task27/ConverterToFahrenheit.java
+++ src/com/alex/lesson5/task27/ConverterToFahrenheit.java
@@ -3,6 +3,6 @@ package com.alex.lesson5.task27;
 public class ConverterToFahrenheit implements Converter {
 
     public static double convert(double num) {
-        return ((num - 32) * 5/9);
+        return ((num * 9/5) + 32);
     }
 }
