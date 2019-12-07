b22102d51b861b11c0757dad51271ca514dc6e02
derwya <39012104+derwya@users.noreply.github.com>
2019-12-07 23:21:20 +0200

    update

--- src/com/alex/lesson6/task30/Main.java
+++ src/com/alex/lesson6/task30/Main.java
@@ -1,6 +1,8 @@
 package com.alex.lesson6.task30;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,17 +15,40 @@ public class Main {
         System.out.println("-------------------------");
 
         shop.addProduct("cat", 1000);
-        shop.addProducts(1000, 1000, 1000);
+        shop.addProducts("cat", 1000, "dog", 1000, "parrot", 1000);
         shop.addProducts("cat", 1000, "parrot", 1000);
 
         System.out.println(shop.shopInfo());
 
         System.out.println("-------------------------");
+        Map<String, Integer> productsMapToAdd = new HashMap<>();
+        productsMapToAdd.put("dog", 10000);
+        productsMapToAdd.put("parrot", 1000);
+        productsMapToAdd.put("cat", 1000);
+
+        shop.addProducts(productsMapToAdd);
+        System.out.println(shop.shopInfo());
+
+        Map<String, Integer> productsMapToBuy = new HashMap<>();
+        productsMapToBuy.put("dog", 100);
+        productsMapToBuy.put("parrot", 100);
+        productsMapToBuy.put("cat", 100);
+
+        System.out.printf("Price is: %f\n\n", shop.buyProducts(productsMapToBuy));
+
+        System.out.println(shop.shopInfo());
+
+        System.out.println("-------------------------");
+
+        shop.addProducts();
+
+
+        System.out.println("-------------------------");
 
 
         System.out.println(shop.buyProduct("cat", 1000));
-        System.out.println(shop.buyProducts(1000, 1000, 1000));
-        System.out.println(shop.buyProducts("cat", 1000, "parrot", 1000));
+        System.out.println(shop.buyProducts("cat", 1000, "dog", 1000, "parrot", 1000));
+        System.out.println(shop.buyProducts("cat", 30, "parrot", 30));
 
         System.out.println("-------------------------");
 
--- src/com/alex/lesson6/task30/Shop.java
+++ src/com/alex/lesson6/task30/Shop.java
@@ -3,82 +3,124 @@ package com.alex.lesson6.task30;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Shop {
 
-    private ArrayList<Product> productList = new ArrayList<>(3);
+    private HashMap<String, Product> productMap = new HashMap<>();
 
     private Shop() {
     }
 
 
-    public Shop(Product product1, Product product2, Product product3) {
-        productList.add(product1);
-        productList.add(product2);
-        productList.add(product3);
-
+    public Shop(Product ...products) {
+        Arrays.asList(products).forEach(product -> this.productMap.put(product.getName(), product));
     }
 
 
-    public Shop(Collection ListOfProduct) {
-        if (ListOfProduct.size() != 3) throw new IllegalArgumentException("There must be 3 products.");
-        productList.addAll(ListOfProduct);
+    public Shop(Collection<Product> listOfProduct) {
+        listOfProduct.forEach(product -> this.productMap.put(product.getName(), product));
     }
 
     public void addProduct(String name, int count) {
-        productList.get(getIndex(name)).addCount(count);
+        Product product = this.productMap.get(name);
+        if(product != null) product.addCount(count);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name));
     }
 
     public void addProducts(String name1, int count1, String name2, int count2) {
-        productList.get(getIndex(name1)).addCount(count1);
-        productList.get(getIndex(name2)).addCount(count2);
+        Product product1 = this.productMap.get(name1);
+        if(product1 != null) product1.addCount(count1);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name1));
+        Product product2 = this.productMap.get(name2);
+        if(product2 != null) product2.addCount(count2);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name2));
+
     }
 
-    public void addProducts(int count1, int count2, int count3) {
-        productList.get(0).addCount(count1);
-        productList.get(1).addCount(count2);
-        productList.get(2).addCount(count3);
+    public void addProducts(String name1, int count1, String name2, int count2, String name3, int count3) {
+        Product product1 = this.productMap.get(name1);
+        if(product1 != null) product1.addCount(count1);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name1));
+        Product product2 = this.productMap.get(name2);
+        if(product2 != null) product2.addCount(count2);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name2));
+        Product product3 = this.productMap.get(name3);
+        if(product3 != null) product2.addCount(count3);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name3));
+    }
+
+    public void addProducts(Map<String, Integer> mapOfProducts) {
+        mapOfProducts.forEach((k, v) -> {
+            Product product = this.productMap.get(k);
+            if(product != null) product.addCount(v);
+            else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", k));
+        });
+
     }
 
     public double buyProduct(String name, int count) {
-        Product product = productList.get(getIndex(name));
-        product.removeCount(count);
+        Product product = this.productMap.get(name);
+        if(product != null) product.removeCount(count);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name));
         return product.getPriceForItem() * count;
     }
 
     public double buyProducts(String name1, int count1, String name2, int count2) {
-        Product product1 = productList.get(getIndex(name1));
-        product1.removeCount(count1);
-        Product product2 = productList.get(getIndex(name2));
-        product2.removeCount(count2);
+        Product product1 = this.productMap.get(name1);
+        if(product1 != null) product1.removeCount(count1);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name1));
+        Product product2 = this.productMap.get(name2);
+        if(product2 != null) product2.removeCount(count2);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name2));
         return product1.getPriceForItem() * count1 + product2.getPriceForItem() * count2;
     }
 
-    public double buyProducts(int count1, int count2, int count3) {
-        Product product1 = productList.get(0);
-        product1.removeCount(count1);
-        Product product2 = productList.get(1);
-        product2.removeCount(count2);
-        Product product3 = productList.get(2);
-        product3.removeCount(count3);
+    public double buyProducts(String name1, int count1, String name2, int count2, String name3, int count3) {
+        Product product1 = this.productMap.get(name1);
+        if(product1 != null) product1.removeCount(count1);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name1));
+        Product product2 = this.productMap.get(name2);
+        if(product2 != null) product2.removeCount(count2);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name2));
+        Product product3 = this.productMap.get(name3);
+        if(product3 != null) product3.removeCount(count3);
+        else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", name3));
         return product1.getPriceForItem() * count1 + product2.getPriceForItem() * count2 + product3.getPriceForItem() * count3;
+    }
+
+    public double buyProducts(Map<String, Integer> mapOfProducts) {
+        double price = 0.0;
+        for (Map.Entry<String, Integer> entry : mapOfProducts.entrySet()) {
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            Product product = this.productMap.get(k);
+            if (product != null) product.removeCount(v);
+            else throw new IllegalArgumentException(String.format("Cannot find product with name '%s'", k));
+            price += product.getPriceForItem() * v;
+        }
+        return price;
     }
 
     public void addProducts() throws IOException {
         do {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Enter product index to add or -1 to stop:");
-            outputProductsList();
-            int index = Integer.parseInt(reader.readLine());
-            if (index == -1) break;
-            if (index < 0 || index > 2) throw new IllegalArgumentException("Unknown product");
+            System.out.println(shopInfo());
+            System.out.println("Enter product name to add or -1 to stop:");
+            String name = reader.readLine();
+            if (name.equals("-1")) break;
             System.out.println("Enter count to add:");
             int count = Integer.parseInt(reader.readLine());
-            Product product = productList.get(index);
+            Product product = this.productMap.get(name);
+            if (product == null) {
+                System.out.println("Cannot find this product");
+                continue;
+            }
             product.addCount(count);
-            System.out.println(count + " " + product.getName() + "s was added to shop!");
+            System.out.printf("%d %ss was added to shop!\n\n", count, product.getName());
         } while (true);
     }
 
@@ -86,49 +128,28 @@ public class Shop {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         double price = 0.0;
         do {
+            System.out.println(shopInfo());
             System.out.println("Enter product index to add to shopping basket or -1 to stop:");
-            outputProductsList();
-            int index = Integer.parseInt(reader.readLine());
-            if (index == -1) break;
-            if (index < 0 || index > 2) throw new IllegalArgumentException("Unknown product");
+            String name = reader.readLine();
+            if (name.equals("-1")) break;
             System.out.println("Enter count to add to shopping basket:");
             int count = Integer.parseInt(reader.readLine());
-            Product product = productList.get(index);
+            Product product = this.productMap.get(name);
             try {
                 product.removeCount(count);
-            } catch (Exception e) {
-                if (e instanceof IllegalArgumentException)
-                    System.out.println(e.toString().replace("java.lang.IllegalArgumentException:", ""));
-                else throw e;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
             price += product.getPriceForItem() * count;
         } while (true);
-        System.out.println("Price is: " + price);
+        System.out.printf("Price is: %f\n\n", price);
         return price;
     }
 
-    private void outputProductsList() {
-        StringBuilder res = new StringBuilder();
-        for (Product product : productList) {
-            res.append(productList.indexOf(product)).append(" - ").append(product.getName()).append("\n");
-        }
-
-
-        System.out.println(res);
-    }
 
     public String shopInfo() {
         StringBuilder res = new StringBuilder("Shop info:\n");
-        for (Product product: productList) {
-            res.append(product.getName()).append("s: ").append(product.getCount()).append("\n");
-        }
+            this.productMap.forEach((k, v) -> res.append(k).append(": ").append(v.getCount()).append("\n"));
         return res.toString();
-    }
-
-    private int getIndex(String name) {
-        for (Product product : productList) {
-            if (name.equals(product.getName())) return productList.indexOf(product);
-        }
-        throw new IllegalArgumentException("Cannot find this product");
     }
 }
