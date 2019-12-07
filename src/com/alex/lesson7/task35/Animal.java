325cb38e6bc645fe6018b29ff289a35e7d783d8a
derwya <39012104+derwya@users.noreply.github.com>
2019-12-07 02:42:57 +0200

    update

---
+++ src/com/alex/lesson7/task35/Animal.java
@@ -0,0 +1,4 @@
+package com.alex.lesson7.task35;
+
+public class Animal {
+}
--- src/com/alex/lesson7/task35/Main.java
+++ src/com/alex/lesson7/task35/Main.java
@@ -1,7 +1,68 @@
 package com.alex.lesson7.task35;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Set<Animal> set1 = createCats();
+        System.out.println("---------------------");
+        printPets(set1);
+        System.out.println("---------------------");
+        Set<Animal> set2 = createDogs();
+        System.out.println("---------------------");
+        printPets(set2);
+        System.out.println("---------------------");
+        Set<Animal> set3 = join(set1, set2);
+        System.out.println("---------------------");
+        printPets(set3);
+        System.out.println("---------------------");
+        Set<Animal> set4 = removeCats(set3);
+        System.out.println("---------------------");
+        printPets(set4);
+        System.out.println("---------------------");
+    }
 
+
+    public static Set<Animal> removeCats(Set<Animal> set) {
+        Set<Animal> setClone = Set.copyOf(set);
+        setClone.forEach(animal -> {
+            if(animal instanceof Cat) set.remove(animal);
+        });
+        return set;
+    }
+
+     public static void printPets(Set<Animal> set) {
+        set.forEach(animal -> System.out.println(animal.toString()));
+    }
+
+    public static Set<Animal> createCats() {
+        Set<Animal> set = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            set.add(new Cat());
+        }
+        return set;
+    }
+
+    public static Set<Animal> createDogs() {
+        Set<Animal> set = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            set.add(new Dog());
+        }
+        return set;
+    }
+
+    public static Set<Animal> join(Set<Animal> set1, Set<Animal> set2) {
+        set1.addAll(set2);
+        return set1;
+    }
+
+
+    public static class Cat extends Animal {
+        public Cat() {}
+    }
+
+    public static class Dog extends Animal {
+        public Dog() {}
     }
 }
