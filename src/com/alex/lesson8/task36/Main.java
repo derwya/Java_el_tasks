21f5a9ada9bf52ddd84cc0ac84132c44952875c7
derwya <39012104+derwya@users.noreply.github.com>
2019-12-09 11:51:13 +0200

    update

--- src/com/alex/lesson8/task36/Main.java
+++ src/com/alex/lesson8/task36/Main.java
@@ -1,7 +1,46 @@
 package com.alex.lesson8.task36;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Method main beginning");
+        methodOne();
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method main ending");
+    }
 
+    public static void methodOne() {
+        System.out.println("Method one beginning");
+        methodTwo();
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method one ending");
+    }
+
+    public static void methodTwo() {
+        System.out.println("Method two beginning");
+        methodThree();
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method two ending");
+    }
+
+    public static void methodThree() {
+        System.out.println("Method three beginning");
+        methodFour();
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method three ending");
+    }
+
+    public static void methodFour() {
+        System.out.println("Method four beginning");
+        methodFive();
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method four ending");
+    }
+
+    public static void methodFive() {
+        System.out.println("Method five beginning");
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        System.out.println("Method five ending");
     }
 }
