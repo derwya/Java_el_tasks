e2553fe449514a57055ec164376c25754edb0b48
derwya <39012104+derwya@users.noreply.github.com>
2019-12-12 23:35:55 +0200

    update

--- src/com/alex/lesson8/task39/Main.java
+++ src/com/alex/lesson8/task39/Main.java
@@ -1,7 +1,6 @@
 package com.alex.lesson8.task39;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
