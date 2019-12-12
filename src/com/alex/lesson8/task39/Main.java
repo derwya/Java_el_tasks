0d491a2c7a8ee5c60ccaa2a72b8c30ebfc068513
derwya <39012104+derwya@users.noreply.github.com>
2019-12-12 23:35:50 +0200

    update

--- src/com/alex/lesson8/task39/Main.java
+++ src/com/alex/lesson8/task39/Main.java
@@ -1,7 +1,19 @@
 package com.alex.lesson8.task39;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(reader.readLine());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd, yyyy");
+            System.out.println(simpleDateFormat.format(date).toUpperCase());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
