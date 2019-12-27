e11e93930de9e262f171b04674f4c1af4301f3d0
derwya <39012104+derwya@users.noreply.github.com>
2019-12-27 20:54:14 +0200

    update

--- src/com/alex/lesson10/task43/Main.java
+++ src/com/alex/lesson10/task43/Main.java
@@ -10,10 +10,10 @@ public class Main {
         Random random = new Random();
 
         for (int i = 0; i < 1000; i++) {
-            cars.add(new Car(Car.BODY_TYPE[random.nextInt(Car.BODY_TYPE.length-1)], Car.BRANDS[random.nextInt(Car.BRANDS.length-1)],  1000 + random.nextInt(10000)));
+            cars.add(new Car(Car.BODY_TYPE[random.nextInt(Car.BODY_TYPE.length-1)], Car.BRANDS[random.nextInt(Car.BRANDS.length-1)],  1000 + random.nextInt(3000)));
         }
 
-        Predicate<Car> isBodySedanAndWeightTwoThousand = car -> car.getBody().equals("Sedan") && car.getWeight() >= 2000;
+        Predicate<Car> isBodySedanAndWeightTwoThousand = car -> car.getBody().equals("Sedan") && car.getWeight() <= 2000;
         ArrayList<Car> sedans = new ArrayList<>();
         cars.forEach(car -> {
             if(isBodySedanAndWeightTwoThousand.test(car)) sedans.add(car);
