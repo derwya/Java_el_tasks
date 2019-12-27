46d65943b7c763e49e4239ea0971f9712d7d52de
derwya <39012104+derwya@users.noreply.github.com>
2019-12-27 20:15:27 +0200

    update

--- src/com/alex/lesson10/task42/Main.java
+++ src/com/alex/lesson10/task42/Main.java
@@ -1,7 +1,15 @@
 package com.alex.lesson10.task42;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(100);
+        Random random = new Random();
+        random.ints(10, 0, 100).forEach(list::add);
 
+        System.out.println(list.toString());
     }
 }
