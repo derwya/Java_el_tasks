6f8505bd16ee992d824e71f24b89692a77ee8892
derwya <39012104+derwya@users.noreply.github.com>
2019-12-27 21:31:36 +0200

    update

---
+++ src/com/alex/lesson10/task45/Format.java
@@ -0,0 +1,5 @@
+package com.alex.lesson10.task45;
+
+public interface Format<T> {
+    void format(T el);
+}
--- src/com/alex/lesson10/task45/Main.java
+++ src/com/alex/lesson10/task45/Main.java
@@ -1,7 +1,17 @@
 package com.alex.lesson10.task45;
 
+
 public class Main {
     public static void main(String[] args) {
+        Format<Object> format = el -> System.out.println("Строка элемента: " + el.toString());
+        testFormat(format, 100);
+        testFormat(format, "qwerty");
+        testFormat(format, 22.01);
+        testFormat(format, new String[]{"test", "test", "test"});
 
+    }
+
+    public static <T> void testFormat(Format<Object> format, T el) {
+        format.format(el);
     }
 }
