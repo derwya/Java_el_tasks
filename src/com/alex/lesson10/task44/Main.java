77db310d4bd98b56afde6ee8ca6244ebb2d6cb0c
derwya <39012104+derwya@users.noreply.github.com>
2019-12-27 21:09:21 +0200

    update

--- src/com/alex/lesson10/task44/Main.java
+++ src/com/alex/lesson10/task44/Main.java
@@ -1,7 +1,23 @@
 package com.alex.lesson10.task44;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
+        Function<String, String> reverse = str -> {
+            String[] strings = str.split("");
+            String[] newStr = new String[strings.length];
+            for (int i = 0; i < strings.length; i++) {
+                newStr[strings.length-i-1] = strings[i];
+            }
+            StringBuilder res = new StringBuilder();
+            for(String s: newStr) {
+                res.append(s);
+            }
 
+            return res.toString();
+        };
+
+        System.out.println(reverse.apply("Привет"));
     }
 }
