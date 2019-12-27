e3af073b992b06ea38b6ed1841c783ce6f3b7aee
derwya <39012104+derwya@users.noreply.github.com>
2019-12-27 20:17:11 +0200

    Update Main.java

--- src/com/alex/lesson10/task42/Main.java
+++ src/com/alex/lesson10/task42/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(100);
+        List<Integer> list = new ArrayList<>();
         Random random = new Random();
         random.ints(10, 0, 100).forEach(el -> list.add(el));
 
